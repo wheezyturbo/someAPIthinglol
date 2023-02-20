e6c1be284b62637dae3e596b9bf795553ea0b735
wheezyturbo <77047619+wheezyturbo@users.noreply.github.com>
2023-02-20 18:33:06 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -268,7 +268,7 @@ app.patch('/update-stock', (req, res) => {
   firebase.firestore().collection('stores').doc(store).collection('items').where('name', '==', itemName).get()
     .then(querySnapshot => {
       if (querySnapshot.empty) {
-        res.status(404).send({ error: 'Item not found in store' });
+        res.status(404).JSON({ error: 'Item not found in store' });
       } else {
         querySnapshot.forEach(doc => {
           firebase.firestore().collection('stores').doc(store).collection('items').doc(doc.id).update({
